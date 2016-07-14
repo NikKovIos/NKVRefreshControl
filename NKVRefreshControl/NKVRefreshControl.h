e4e6bc3ea977c72a7bf4dfd690b5ba67b828b743
Nik Kov <n.kovalenok@inmoso.ru>
2016-07-14 13:09:17 +0300

    Added RefreshControl class

---
+++ NKVRefreshControl/NKVRefreshControl.h
@@ -0,0 +1,31 @@
+//
+//  NKVRefreshControl.h
+//  NKVRefreshControlExample
+//
+//  Created by Nik Kov on 14.07.16.
+//  Copyright © 2016 Nik Kov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NKVRefreshControl : UIView
+
+/**
+ Add Pull to refresh to ScrollView with delegate controller (typically 'self') and action selector. Pull to refresh call selector when user pull ScrollView (UITableView) down. Typically it uses to load data.
+ */
++ (NKVRefreshControl *)addPullToRefreshToScrollView:(UIScrollView *)scrollView delegate:(id)delegate pullToRefreshAction:(SEL)pullToRefreshAcrtion;
+
+/**
+ Add Infinity Scroll to ScrollView with delegate controller (typically 'self') and action selector. Infinity scroll call selector, when user srolls down to bottom of ScrollView.
+ */
++ (NKVRefreshControl *)addInfinityScrollToScrollView:(UIScrollView *)scrollView delegate:(id)delegate infinityScrollAction:(SEL)infinityScrollAction;
+
+/**
+ Add both Pull to refresh and Infinity Scroll by one method. You also can do it separately.
+ */
++ (NKVRefreshControl *)addBOTHRefreshControlsToScrollView:(UIScrollView *)scrollView delegate:(id)delegate pullToRefreshAction:(SEL)pullToRefreshAcrtion;
+
+
+
+
+@end
---
+++ NKVRefreshControl/NKVRefreshControl.m
@@ -0,0 +1,13 @@
+//
+//  NKVRefreshControl.m
+//  NKVRefreshControlExample
+//
+//  Created by Nik Kov on 14.07.16.
+//  Copyright © 2016 Nik Kov. All rights reserved.
+//
+
+#import "NKVRefreshControl.h"
+
+@implementation NKVRefreshControl
+
+@end
